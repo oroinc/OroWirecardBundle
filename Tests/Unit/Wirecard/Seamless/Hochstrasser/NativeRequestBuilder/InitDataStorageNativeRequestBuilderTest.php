bc141598e4c3aa888bb43622ea01c1c26a31aca0
Anton Khristiansen <akhristiansen@oroinc.com>
2017-08-11 19:05:44 +0300

    BB-9679: Cover by unit tests wirecard Wirecard/Seamless/Hochstrasser classes (#12526)
    * BB-9679: Cover by unit tests wirecard Wirecard/Seamless/Hochstrasser classes
    - added unit test for Gateway & NativeRequestBuilder

---
+++ Tests/Unit/Wirecard/Seamless/Hochstrasser/NativeRequestBuilder/InitDataStorageNativeRequestBuilderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Oro\Bundle\WirecardBundle\Tests\Unit\Wirecard\Seamless\Hochstrasser\NativeRequestBuilder;
+
+use Hochstrasser\Wirecard\Request\Seamless\Frontend\InitDataStorageRequest as WirecardInitDataStorageRequest;
+
+use Oro\Bundle\WirecardBundle\Wirecard\Seamless\Hochstrasser\NativeRequestBuilder\InitDataStorageNativeRequestBuilder;
+use Oro\Bundle\WirecardBundle\Wirecard\Seamless\Request\InitDataStorageRequest;
+use Oro\Bundle\WirecardBundle\Wirecard\Seamless\Option;
+
+class InitDataStorageNativeRequestBuilderTest extends AbstractNativeRequestBuilderTest
+{
+    public function setUp()
+    {
+        $this->options[Option\OrderIdent::ORDERIDENT] = 'indent';
+        $this->options[Option\ReturnUrl::RETURNURL] = 'returnUrl';
+        $this->nativeRequestBuilder = new InitDataStorageNativeRequestBuilder();
+    }
+
+    /** {@inheritdoc} */
+    public function expectedClassDataProvider(): array
+    {
+        return [[WirecardInitDataStorageRequest::class]];
+    }
+
+    /** {@inheritdoc} */
+    public function expectedRequestIdentifierDataProvider(): array
+    {
+        return [[InitDataStorageRequest::IDENTIFIER]];
+    }
+}
