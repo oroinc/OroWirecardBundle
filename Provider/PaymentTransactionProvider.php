12775f7565f6a2915387270a22bb26a69ca14af0
oro-panasiukr <39378223+oro-panasiukr@users.noreply.github.com>
2018-12-04 18:50:27 +0200

    BB-13976: Payment methods: guest user is not able finish the checkout, 500 error on submit order (#20912)
    - fixed logic in PaymentTransactionProvider when Token contains only CustomerVisitor object
    - fixed checkout must be created with empty customerUser for guests
    - updated related behat scenarios according to new behavior
    - added behat test for guest checkout using Authorize.Net, Apruve, InfinitePay and PayPal
    - added mocks for behat tests for InfinitePay and Apruve
    - fixed hmac dependence

--- Provider/PaymentTransactionProvider.php
+++ Provider/PaymentTransactionProvider.php
@@ -2,10 +2,15 @@
 
 namespace Oro\Bundle\WirecardBundle\Provider;
 
+use Oro\Bundle\CustomerBundle\Entity\CustomerUser;
 use Oro\Bundle\PaymentBundle\Entity\PaymentTransaction;
 use Oro\Bundle\PaymentBundle\Provider\PaymentTransactionProvider as BasePaymentTransactionProvider;
 use Oro\Bundle\WirecardBundle\Method\WirecardSeamlessInitiateAwarePaymentMethodInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
+/**
+ * This provider searches active initiate payment transaction
+ */
 class PaymentTransactionProvider extends BasePaymentTransactionProvider
 {
     /**
@@ -15,7 +20,10 @@ class PaymentTransactionProvider extends BasePaymentTransactionProvider
      */
     public function getActiveInitiatePaymentTransaction($object, $paymentMethod)
     {
-        $customerUser = $this->getLoggedCustomerUser();
+        $customerUser = $this->customerUserProvider
+            ? $this->customerUserProvider->getUser(false)
+            : $this->getLoggedCustomerUser();
+
         if (!$customerUser) {
             return null;
         }
@@ -28,5 +36,23 @@ class PaymentTransactionProvider extends BasePaymentTransactionProvider
         ];
 
         return $this->getPaymentTransaction($object, $criteria);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getLoggedCustomerUser()
+    {
+        $token = $this->tokenStorage->getToken();
+        if (!$token instanceof TokenInterface) {
+            return null;
+        }
+
+        $user = $token->getUser();
+        if ($user instanceof CustomerUser) {
+            return $user;
+        }
+
+        return null;
     }
 }
