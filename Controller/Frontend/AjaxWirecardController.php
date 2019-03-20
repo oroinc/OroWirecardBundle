9efe0b55743ae3be2df7594d53d1ec3e438374d5
Yurii Muratov <ymuratov@oroinc.com>
2019-03-20 17:54:43 +0200

    BAP-16112: non idempotent requests  (#22611)

--- Controller/Frontend/AjaxWirecardController.php
+++ Controller/Frontend/AjaxWirecardController.php
@@ -5,12 +5,17 @@ namespace Oro\Bundle\WirecardBundle\Controller\Frontend;
 use Oro\Bundle\CheckoutBundle\Entity\Checkout;
 use Oro\Bundle\PaymentBundle\Method\PaymentMethodInterface;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Oro\Bundle\SecurityBundle\Annotation\CsrfProtection;
 use Oro\Bundle\WirecardBundle\Method\WirecardSeamlessInitiateAwarePaymentMethodInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
+/**
+ * Ajax Wirecard Controller
+ */
 class AjaxWirecardController extends Controller
 {
     /**
@@ -20,6 +25,8 @@ class AjaxWirecardController extends Controller
      *      requirements={"id"="\d+"}
      * )
      * @AclAncestor("oro_checkout_frontend_checkout")
+     * @Method("POST")
+     * @CsrfProtection()
      *
      * @param Checkout $checkout
      * @param PaymentMethodInterface $paymentMethod
