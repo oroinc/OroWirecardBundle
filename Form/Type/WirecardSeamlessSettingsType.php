d37071d9657387c77f87191c719ca16b19c719eb
Yefim Yevtushenko <webevt@users.noreply.github.com>
2019-03-22 18:58:51 +0200

    BAP-18172: Disable autocomplete in customer registration form (#23056)
    - added autocomplete off to password inputs
    - set autocomplete off on user, customer user login pages
    - disabled autocomplete in integrations forms
    - added unit test

--- Form/Type/WirecardSeamlessSettingsType.php
+++ Form/Type/WirecardSeamlessSettingsType.php
@@ -13,6 +13,9 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+/**
+ * Wirecard Seamless integration settings form type.
+ */
 class WirecardSeamlessSettingsType extends AbstractType
 {
     const BLOCK_PREFIX = 'oro_wirecard_seamless_settings';
@@ -66,7 +69,6 @@ class WirecardSeamlessSettingsType extends AbstractType
             ->add('secret', OroEncodedPlaceholderPasswordType::class, [
                 'label' => 'oro.wirecard.settings.wirecard_seamless.secret.label',
                 'required' => true,
-                'attr' => ['autocomplete' => 'new-password'],
             ])
             ->add('wcTestMode', CheckboxType::class, [
                 'label' => 'oro.wirecard.settings.wirecard_seamless.test_mode.label',
